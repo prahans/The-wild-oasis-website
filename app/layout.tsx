cf2b9d19b7bbb1efc0c73bd44a4cded099d06212
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-08 05:20:18 +0545

    testing the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
 import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: {
+    default: "The Wild Oasis",
+    template: "%s | The Wild Oasis",
+  },
+  description: "Welcome to The Wild Oasis.",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
