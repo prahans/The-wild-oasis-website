a2c4ef30b5f52716d4a32f62d54302087a9b5603
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-08 09:38:28 +0545

    Fetching Data in a Page

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,17 @@
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
+  const users = await res.json();
   return (
     <>
       <h1>Cabins page</h1>
+      <ul>
+        {users.map((user: { id: number; name: string }) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
