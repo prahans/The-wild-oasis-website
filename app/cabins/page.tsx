3d586bb0e4f3de3de0ca4ff698a3234c5a63bc6f
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-08 14:35:23 +0545

    Displaying a Loading Indicator

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,6 +1,9 @@
 import Counter from "../components/Counter";
 
 export default async function Page() {
+  // Temporary delay to make the loading UI visible during testing.
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   if (!res.ok) {
     throw new Error("Failed to fetch users");
