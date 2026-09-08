e5e245fea8f559be1a158925c7691a49c22926e7
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-08 18:36:38 +0545

    Optimizing Images With Next.js <Image /> Component

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img height="60" width="60" alt="The Wild Oasis logo" src="/logo.png" />
+      <Image src={logo} alt="The Wild Oasis logo" height="60" width="60" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
