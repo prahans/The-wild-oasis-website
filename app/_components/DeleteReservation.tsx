4253e69192e2c765d06f7fdbc305d3be01a5b6ec
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-10 16:22:14 +0545

    Deleting a Reservation

--- app/_components/DeleteReservation.tsx
+++ app/_components/DeleteReservation.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { TrashIcon } from "@heroicons/react/24/solid";
 import type { RecordId } from "@/app/_types/data";
+import { deleteReservation } from "../_lib/actions";
 
 function DeleteReservation({ bookingId }: { bookingId: RecordId }) {
   return (
     <button
+      onClick={() => deleteReservation(bookingId)}
       aria-label={`Delete reservation ${bookingId}`}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -4,6 +4,8 @@ import { revalidatePath } from "next/cache";
 import { auth, signOut } from "./auth";
 import { signIn } from "./auth";
 import { getSupabase } from "./supabase";
+import type { RecordId } from "@/app/_types/data";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData: FormData): Promise<void> {
   const session = await auth();
@@ -40,6 +42,26 @@ export async function updateGuest(formData: FormData): Promise<void> {
 
   if (error) throw new Error("Guest could not be updated");
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId: RecordId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+  const guestId = session.user.guestId;
+  if (guestId === undefined)
+    throw new Error("Guest profile could not be found");
+
+  const guestBookings = await getBookings(guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(Number(bookingId)))
+    throw new Error("You are not allowed to delete this booking");
+  const { error } = await getSupabase()
+    .from("bookings")
+    .delete()
+    .eq("id", Number(bookingId));
+
+  if (error) throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
