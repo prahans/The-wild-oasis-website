cb9b3e613c9d5e56ca7ed2935593aa5b73fea472
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-10 08:02:53 +0545

    Getting the User Session

--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -2,12 +2,15 @@ import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import type { Cabin } from "@/app/_types/data";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import { auth } from "../_lib/auth";
+import LoginMessage from "./LoginMessage";
 
 export default async function Reservation({ cabin }: { cabin: Cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-100">
       <DateSelector
@@ -15,7 +18,11 @@ export default async function Reservation({ cabin }: { cabin: Cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
--- app/_components/ReservationForm.tsx
+++ app/_components/ReservationForm.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import type { Cabin } from "@/app/_types/data";
+import type { User } from "next-auth";
 
-function ReservationForm({ cabin }: { cabin: Cabin }) {
+function ReservationForm({ cabin, user }: { cabin: Cabin; user: User }) {
   // CHANGE
   const { maxCapacity } = cabin;
 
@@ -11,16 +12,16 @@ function ReservationForm({ cabin }: { cabin: Cabin }) {
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <img
             // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
+            referrerPolicy="no-referrer"
+            className="h-8 rounded-full"
+            src={user.image ?? undefined}
+            alt={user.name ?? "User avatar"}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
