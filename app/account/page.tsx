71ee678a41ee5b85d18bdd6ceeb651c0b1268a2d
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-10 08:47:00 +0545

    Building a Custom Sign In Page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,17 @@
 import type { Metadata } from "next";
+import { auth } from "@/app/_lib/auth";
 
 export const metadata: Metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session?.user?.name?.split(" ").at(0) ?? "Guest";
   return (
     <>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Prahans
+        Welcome, {firstName}
       </h2>
     </>
   );
